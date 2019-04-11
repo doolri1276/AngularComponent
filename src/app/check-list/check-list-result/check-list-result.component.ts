e1054423abdc3281310d289f71725e60e44a82fc
doolri1276 <doolri1277@gmail.com>
2019-04-11 17:52:25 +0900

    - 예제 6-14 set 메소드를 사용한 프로퍼티 바인딩

--- src/app/check-list/check-list-result/check-list-result.component.ts
+++ src/app/check-list/check-list-result/check-list-result.component.ts
@@ -8,37 +8,21 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 //ng g component check-list-result
 export class CheckListResultComponent implements OnInit {
-  @Input() checkedResult: string[];
-
+  _checkedData: string[];
   checkedCnt: number;
 
-  constructor() { 
-    // this.initResult();
-    // const buttonElem = document.querySelector('button');
-    // buttonElem.addEventListener('click', () => this.collectCheckedResult());
+  constructor() { }
+
+  @Input() 
+  set checkedResult(checkedResult: string[]){
+    if(!checkedResult){
+      return;
+    }
+    this._checkedData = checkedResult;
+    this.checkedCnt = this._checkedData.length;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
-  // private initResult() {
-  //   this.checkedCnt = 0;
-  //   this.checkedResult = [];
-  // }
-
-  // private collectCheckedResult(){
-  //   this.initResult();
-  //   const spanElems = document.querySelectorAll('span');
-  //   for(let i=0;i<spanElems.length; i++){
-  //     const spanElem = spanElems.item(i);
-
-  //     const checkboxElem = spanElem.querySelector('input');
-  //     if(checkboxElem.checked) {
-  //       this.checkedResult.push(spanElem.querySelector('label').innerText);
-  //     }
-  //   }
-
-  //   this.checkedCnt = this.checkedResult.length;
-  // }
 
 }
