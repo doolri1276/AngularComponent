243fe660219cb0e70e856fe26c83d1e2ad3c76e1
doolri1276 <doolri1277@gmail.com>
2019-04-11 16:44:00 +0900

    - 예제 6-6 DOM API로 접근하기, 하지만 되도록 쓰면 안됨

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CheckListComponent } from './check-list/check-list.component';
-import { CheckListResultComponent } from './check-list-result/check-list-result.component';
+import { CheckListResultComponent } from './check-list/check-list-result/check-list-result.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CheckListResultComponent } from './check-list-result/check-list-result.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
